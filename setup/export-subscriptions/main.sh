09783254102095c6ceca46a145d97d0e8050bda1
ryoma-nagata <ryoma.nagata0713.b@gmail.com>
2024-03-14 00:50:58 +0900

    - 月次エクスポートの追加 - 上書きと初回実行を追加

--- setup/export-subscriptions/main.sh
+++ setup/export-subscriptions/main.sh
@@ -16,10 +16,24 @@ then
   read containerName
 fi
 
+if [ -z "$check_execute_month" ]
+then
+  echo "先月のコスト出力を実行する場合は、yesを入力してください。それ以外の場合は何も入力せずにEnterを押してください:"
+  read check_execute_month
+fi
+
+if [ -z "$check_execute_overwrite" ]
+then
+  echo "既存のエクスポート設定を上書きする場合は、yesを入力してください。それ以外の場合は何も入力せずにEnterを押してください:"
+  read check_execute_overwrite
+fi
+
 while IFS=, read subscriptionId
 do
     SUBSCRIPTION_ID=$subscriptionId \
     STORAGE_ID=$storage_id \
     CONTAINER_NAME=$containerName \
+    CHECK_EXECUTE_MONTH=$check_execute_month \
+    CHECK_EXECUTE_OVERWRITE=$check_execute_overwrite \
         bash ./scripts/costexport_subscription.sh
 done < subscriptions.csv
--- setup/export-subscriptions/scripts/costexport_subscription.sh
+++ setup/export-subscriptions/scripts/costexport_subscription.sh
@@ -10,6 +10,8 @@ set -o nounset
 subscriptionId=$SUBSCRIPTION_ID
 storageId=$STORAGE_ID
 containerName=$CONTAINER_NAME
+check_execute_month=$CHECK_EXECUTE_MONTH
+check_execute_overwrite=$CHECK_EXECUTE_OVERWRITE
 
 ##############################################################
 # 定数
@@ -17,11 +19,11 @@ version='2023-04-01-preview'
 baseUrl=https://management.azure.com
 ##############################################################
 # 変数
-exportName=${subscriptionId}_daily
-targetURL=${baseUrl}/subscriptions/${subscriptionId}/providers/Microsoft.CostManagement/exports/${exportName}?api-version=${version}
+exportName_daily=${subscriptionId}_daily
+targetURL_daily=${baseUrl}/subscriptions/${subscriptionId}/providers/Microsoft.CostManagement/exports/${exportName_daily}?api-version=2023-04-01-preview
 current=$(date -u +"%Y-%m-%dT%H:%M:%S.000Z")
 
-body=$(printf '{
+body_daily=$(printf '{
     "properties": {
         "definition": {
             "dataSet": {
@@ -34,7 +36,7 @@ body=$(printf '{
         "deliveryInfo": {
             "destination": {
                 "container": "%s",
-                "rootFolderPath": "export",
+                "rootFolderPath": "export", 
                 "resourceId": "%s"
             }
         },
@@ -56,16 +58,79 @@ body=$(printf '{
     "location": "global"
 }' $containerName $storageId $current)
 
+exportName_monthly=${subscriptionId}_monthly
+targetURL_monthly=${baseUrl}/subscriptions/${subscriptionId}/providers/Microsoft.CostManagement/exports/${exportName_monthly}?api-version=2023-04-01-preview
+targetURLrun_monthly=${baseUrl}/subscriptions/${subscriptionId}/providers/Microsoft.CostManagement/exports/${exportName_monthly}/run?api-version=2023-11-01
+body_monthly=$(printf '{
+    "properties": {
+        "definition": {
+            "dataSet": {
+                "granularity": "Daily",
+                "grouping": []
+            },
+            "timeframe": "TheLastBillingMonth",
+            "type": "ActualCost"
+        },
+        "deliveryInfo": {
+            "destination": {
+                "container": "%s",
+                "rootFolderPath": "export", 
+                "resourceId": "%s"
+            }
+        },
+        "format": "Csv",
+        "partitionData": true,
+        "schedule": {
+            "recurrence": "Monthly",
+            "recurrencePeriod": {
+                "from": "%s",
+                "to": "2050-01-01T00:00:00.000Z"
+            },
+            "status": "Active"
+        }
+    },
+    "type": "Microsoft.CostManagement/reports",
+    "identity": {
+        "type": "systemAssigned"
+    },
+    "location": "global"
+}' $containerName $storageId $current)
+
 ##############################################################
 
 # echo "targetURL: $targetURL"
 # echo "body: $body"
-echo "サブスクリプションID: ${subscriptionId} のエクスポート構成を作成します"
+echo "サブスクリプションID: ${subscriptionId} について、毎日、当月分のコストを出力するエクスポート構成を作成します..."
 # 既存のexportを取得して、存在しない場合にputを実行する
 
-resource=$(az rest --method get --url "$targetURL" 2>/dev/null || true) # "get"操作を試みる
-if [ -z "$resource" ]; then
-    az rest --method put --url "$targetURL" --body "$body"
+resource_daily=$(az rest --method get --url "$targetURL_daily" 2>/dev/null || true) # "get"操作を試みる
+
+if [ -z "$resource_daily" ]; then
+    az rest --method put --url "$targetURL_daily" --body "$body_daily"
 else
-  echo "このサブスクリプションはすでにエクスポート構成済みです"
+  echo "このサブスクリプションはすでにエクスポート構成済みです。"
+  if [ "$check_execute_overwrite" = "yes" ]; then
+    echo "既存のエクスポート構成を上書きします..."
+    az rest --method put --url "$targetURL_daily" --body "$body_daily"
+  fi
+fi
+
+echo "サブスクリプションID: ${subscriptionId} について、毎月、前月分のコストを出力するエクスポート構成を作成します..."
+
+resource_monthly=$(az rest --method get --url "$targetURL_monthly" 2>/dev/null || true) # "get"操作を試みる
+
+if [ -z "$resource_monthly" ]; then
+    az rest --method put --url "$targetURL_monthly" --body "$body_monthly"
+else
+  echo "このサブスクリプションはすでにエクスポート構成済みです。"
+  if [ "$check_execute_overwrite" = "yes" ]; then
+    echo "既存のエクスポート構成を上書きします..."
+    az rest --method put --url "$targetURL_monthly" --body "$body_monthly"
+  fi
+fi
+
+if [ "$check_execute_month" = "yes" ]; then
+  echo "先月分のコスト出力を指示します..."
+  az rest --method post --url "$targetURLrun_monthly"
+  echo "先月分のコスト出力が開始されました。ストレージに出力されるまで時間がかかります..."
 fi
